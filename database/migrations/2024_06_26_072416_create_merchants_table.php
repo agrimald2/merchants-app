19a393ccf19c755366ca06d7f35fef7054ef4e8c
agrimal2 <agrimaldopci18@gmail.com>
2024-06-26 02:59:15 -0500

    Update 2024_06_26_072416_create_merchants_table.php

--- database/migrations/2024_06_26_072416_create_merchants_table.php
+++ database/migrations/2024_06_26_072416_create_merchants_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('dni');
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
